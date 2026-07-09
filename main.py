648a5e6ae6b1b03b4cbc658bb7b8a44c4c44ebdb
Leandro Costa Lima Verde <leandroclv@gmail.com>
2026-07-09 18:04:45 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#importar as bibliotecas
+import streamlit as st
+import pandas as pd
+
+
+#Criar as funções de carregamento de dados
+    #Cotações do Itau - ITUB4 - 2010 a 2024
+    
+    
+    
+#Preparar as visualizações
+
+
+
+#Criar a interface do streamlit
